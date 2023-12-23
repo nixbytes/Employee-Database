8bbdb282d093bb93f4fbaeb92fdd1a32f93dacd6
nixbytes <real8686@gmail.com>
2023-12-22 21:17:22 -0500

    trying out struct

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
+#define MAX_IDS 32
+
+struct employee_t{
+
+    int id;
+    char firstname[64];
+    char lastname[64];
+    float income;
+
+}
+
+
 int main() {
 
   int personID = 1;
   printf("%d", personID);
+  struct employee_t joe;
+  joe.firstname = "joe";
+  joe.lastname = "brew";
+  joe.income = 1560.50;
+
   return 0;
 }
