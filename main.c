618f4e5da061ea892c8026b2c6ac1cc7f5b22a8e
nixbytes <real8686@gmail.com>
2024-01-15 22:45:28 -0500

    Work on Pointers

--- main.c
+++ main.c
@@ -2,24 +2,32 @@
 
 #define MAX_IDS 32
 
-struct employee_t{
+struct employee_t {
 
-    int id;
-    char firstname[64];
-    char lastname[64];
-    float income;
+  int id;
+  char firstname[64];
+  char lastname[64];
+  float income;
+  bool staff;
+};
 
+void initialize_employee(struct employee_t *e) {
+
+  e->id = 0;
+  e->income = 0;
+  e->staff = false;
+
+  return;
 }
-
 
 int main() {
 
-  int personID = 1;
-  printf("%d", personID);
+  // int personID = 1;
+  // printf("%d", personID);
   struct employee_t joe;
-//  joe.firstname = "joe";
-//  joe.lastname = "brew";
-  joe.income = 1560.50;
+  //  joe.firstname = "joe";
+  //  joe.lastname = "brew";
+  // joe.income = 1560.50;
 
   return 0;
 }
