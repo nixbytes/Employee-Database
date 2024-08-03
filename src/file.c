91fd52211b455e410950249876f10bf3917dec55
nixbytes <real8686@gmail.com>
2024-08-02 19:06:25 -0500

    add logic for fd

--- src/file.c
+++ src/file.c
@@ -1,5 +1,26 @@
-#include <fcntl.h>
 #include <stdio.h>
+
+#include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
+#include <common.h>
+#include <file.h>
+
+int create_db_file(char *filename) {
+  int fd = open(filename, O_RDONLY);
+  // Review and handle logic of file
+  if (fd != -1) {
+    close(fd);
+    printf("File already exists\n");
+    return STATUS_ERROR;
+  }
+  // open the file
+  fd = open(filename, O_RDWR | O_CREATE, 0644);
+  if (fd == -1) {
+    perror("open");
+    return STATUS_ERROR;
+  }
+  return fd;
+}
