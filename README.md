259d08b7ea68e8f79b3f083952e841a9b119ecc1
nixbytes <real8686@gmail.com>
2025-05-08 19:23:15 -0400

    clean up and add testing

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Employee-Database
 Employee Database example in C from Low Level Academy
+
+### Tips for easier life
+
+Using Strict complier flags
+
+```shell
+clang -std=c99 -Wall -Werror main.c
+```
+
+### Unity Build systems
+
+use save temp flag to get a better understand on how its builds
+
+```shell
+clang --save-temps
+```
--- src/main.c
+++ src/main.c
@@ -1,59 +1,17 @@
-#include <stdbool.h>
 #include <stdio.h>
-// #include <stdlib.h>
-#include <getopt.h>
 
-//#include "common.h"
-#include "file.h"
+int main(){
 
-#define MAX_IDS 32
+int employeeID[5];
 
-void print_usage(char *argv[]) {
-  printf("Usage: %s -n -f <database file\n>", argv[0]);
-  printf("\t -n - create new database file\n");
-  printf("\t -f - (required) path to database file\n");
-  return;
-}
+employeeID[0] = 13;
+employeeID[1] = 12;
+employeeID[2] = 1;
+employeeID[3] = 121212;
+employeeID[4] = 10000;
 
-int main(int argc, char *argv[]) {
-  char *filepath = NULL;
-  bool newfile = false;
-  int c;
-  int dbfd = -1;
-
-  while ((c = getopt(argc, argv, "nf:")) != -1) {
-    switch (c) {
-
-    case 'n':
-      newfile = true;
-      break;
-    case 'f':
-      filepath = optarg;
-      break;
-    case '?':
-      printf("Unknown options -c%c\n", c);
-      break;
-    default:
-      return -1;
-    }
-  }
-
-  if (filepath == NULL) {
-    printf("Filepath is a required argument\n");
-    print_usage(argv);
-
-    return 0;
-  }
-
-  if (newfile){
-
-    dbfd = create_db_file(filepath);
-
-  }
-  
-  printf("Newfile: %d\n", newfile);
-  printf("Filepath: %s\n", filepath);
-
+printf("{%d}", employeeID[2]);
 
 return 0;
+
 }
